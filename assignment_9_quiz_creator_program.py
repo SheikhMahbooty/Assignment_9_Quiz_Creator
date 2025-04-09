2f7f4ac17965523fc9a5af3c0ec67ce7b28f338e
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-04-09 15:49:32 +0800

    initial pseudocode for the assignment, with some (cool) comments

---
+++ assignment_9_quiz_creator_program.py
@@ -0,0 +1,10 @@
+#Assignment 9: Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
+#Create loop so the user can input a question
+#"Does your mom know you're gay?"
+#Make user input 4 answers with a correct answer
+#"a. Yes b. No c. Maybe d. Not yet"
+#Make choices to make new questions or quit
+#Would you like to make a new question? or quit
+#Export to txt file (I DO NOT KNOW HOW!!!!!)
+#Add charisma and rizz up all the huzz (optional)
+#Start a fight club
