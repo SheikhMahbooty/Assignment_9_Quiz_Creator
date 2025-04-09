f05c5ff6907ae2796b730129caef009c5c5c99bd
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-04-09 16:08:57 +0800

    Created the loop (although only for the inputted question) to quit the program or continue

--- assignment_9_quiz_creator_program.py
+++ assignment_9_quiz_creator_program.py
@@ -1,8 +1,10 @@
 #Assignment 9: Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
 
-#Add variable for question
 #Create loop so the user can input a question
-input_question = input("Enter a question: ")
+while True:
+    input_question = input("Enter a question or type 'exit' to exit: ") #Created question loop to exit or add a question
+    if input_question.lower() == "exit":
+        break
 #Changed variables to "answer_a... answer_d"
 answer_a = input("Enter answer a: ")
 answer_b = input("Enter answer b: ")
@@ -13,5 +15,3 @@ answer_d = input("Enter answer d: ")
 print(answer_a, answer_b, answer_c, answer_d)
 print(input_question)
 #Export to txt file (I DO NOT KNOW HOW!!!!!)
-#Add charisma and rizz up all the huzz (optional)
-#Start a fight club
