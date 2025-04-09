2b3cecf6457783dd5f15ed8723ae34ebab2dd9ed
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-04-09 15:58:39 +0800

    added initial code for the input and questions

--- assignment_9_quiz_creator_program.py
+++ assignment_9_quiz_creator_program.py
@@ -1,11 +1,18 @@
 #Assignment 9: Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
+
 #Add variable for question
 #Create loop so the user can input a question
+input_question = input("Enter a question: ")
 #"Does your mom know you're gay?"
 #Make user input 4 answers with a correct answer
 #"a. Yes b. No c. Maybe d. Not yet"
+answer_1 = input("Enter answer 1: ")
+answer_2 = input("Enter answer 2: ")
+answer_3 = input("Enter answer 3: ")
+answer_4 = input("Enter answer 4: ")
 #Make choices to make new questions or quit
 #Would you like to make a new question? or quit
+print(answer_1, answer_2, answer_3, answer_4)
 #Export to txt file (I DO NOT KNOW HOW!!!!!)
 #Add charisma and rizz up all the huzz (optional)
 #Start a fight club
