994ac1262b1c2efc900bcad4b94d04f557a69878
Hassan Mustafa <hassanmustafa030107@gmail.com>
2025-04-09 15:53:45 +0800

    updated comments/ added comment

--- assignment_9_quiz_creator_program.py
+++ assignment_9_quiz_creator_program.py
@@ -1,4 +1,5 @@
 #Assignment 9: Create a program that ask user for a question, it will also ask for 4 possible answer (a,b,c,d) and the correct answer. Write the collected data to a text file. Ask another question until the user chose to exit.
+#Add variable for question
 #Create loop so the user can input a question
 #"Does your mom know you're gay?"
 #Make user input 4 answers with a correct answer
